d533ee8ebdca217d484a9602d8f425e349c7668b
RASIT <53307918+Rasit-NP@users.noreply.github.com>
2025-07-29 12:57:42 +0900

    [Silver II] Title: 최대 힙, Time: 116 ms, Memory: 38340 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.py
@@ -0,0 +1,13 @@
+import sys, heapq
+
+n = int(sys.stdin.readline())
+heap = []
+for _ in range(n):
+    order = int(sys.stdin.readline())
+    if order == 0:
+        if len(heap)==0:
+            print(0)
+        else:
+            print(abs(heapq.heappop(heap)))
+    else:
+        heapq.heappush(heap,-order)
