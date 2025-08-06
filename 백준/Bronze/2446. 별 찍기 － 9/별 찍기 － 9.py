33a333632dfed706626363302825e2b8348a559b
RASIT <53307918+Rasit-NP@users.noreply.github.com>
2025-08-06 16:10:27 +0900

    [Bronze III] Title: 별 찍기 - 9, Time: 92 ms, Memory: 108384 KB -BaekjoonHub

---
+++ 백준/Bronze/2446. 별 찍기 － 9/별 찍기 － 9.py
@@ -0,0 +1,13 @@
+import sys
+
+n = int(sys.stdin.readline())
+
+def printing(a):
+    ans = ' '*(n-a)
+    ans += '*'*(2*a-1)
+    print(ans)
+
+for i in range(n, 0, -1):
+    printing(i)
+for i in range(2, n+1):
+    printing(i)
