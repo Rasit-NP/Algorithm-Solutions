95aa4d06fe92e81fd0e6e4e343ed6319b46649cf
RASIT <53307918+Rasit-NP@users.noreply.github.com>
2025-08-14 15:40:45 +0900

    [Gold II] Title: 피보나치 수 3, Time: 92 ms, Memory: 108384 KB -BaekjoonHub

---
+++ 백준/Gold/2749. 피보나치 수 3/피보나치 수 3.py
@@ -0,0 +1,32 @@
+import sys
+
+def product(matrixa, matrixb, mod=1000000):
+    ans = [[None, None],[None, None]]
+    for i in range(2):
+        for j in range(2):
+            sum = 0
+            for k in range(2):
+                sum += matrixa[i][k]*matrixb[k][j]
+            ans[i][j] = sum % mod
+    return ans
+
+def square(matrix):
+    return product(matrix, matrix)
+
+n = int(sys.stdin.readline())
+
+Q = [[1, 1], [1, 0]]
+binary = []
+
+total = [[1, 0], [0, 1]]
+
+while n:
+    binary.append(n%2)
+    n //= 2
+
+for i in range(len(binary)):
+    if binary[i] == 1:
+        total = product(total, Q)
+    Q = square(Q)
+
+print(total[1][0])
