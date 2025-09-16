9d2d9691b02e3ff836450b3601f6a5df27b5622b
RASIT <53307918+Rasit-NP@users.noreply.github.com>
2025-09-16 11:27:07 +0900

    [D4] Title: [S/W 문제해결 응용] 4일차 - 보급로, Time: 230 ms, Memory: 66,432 KB -BaekjoonHub

--- SWEA/D4/1249. ［S／W 문제해결 응용］ 4일차 － 보급로/［S／W 문제해결 응용］ 4일차 － 보급로.py
+++ SWEA/D4/1249. ［S／W 문제해결 응용］ 4일차 － 보급로/［S／W 문제해결 응용］ 4일차 － 보급로.py
@@ -2,30 +2,27 @@ import heapq
 
 t = int(input())
 
-for testcase in range(1, t+1):
+for tc in range(1, t+1):
     n = int(input())
-    board = []
-    heap = [(0,[0,0])]          # (time, [x, y])
-    visited = [[float('inf')]*n for _ in range(n)]
+
+    board = [list(map(int, list(input().rstrip()))) for _ in range(n)]
+    heap = []
+    visited = [[float('inf') for _ in range(n)] for _ in range(n)]
+
+    heap.append((0, (0, 0)))
     visited[0][0] = 0
-    
-    for _ in range(n):
-        board.append(list(map(int,list(input()))))
 
     while heap:
-        now = heapq.heappop(heap)
-        spenttime = now[0]
-        x = now[1][0]
-        y = now[1][1]
-        if x==n-1 and y == n-1:
-            print(f'#{testcase} {spenttime}')
+        dist, posi = heapq.heappop(heap)
+        x, y = posi
+        if x == n-1 and y == n-1:
+            ans = dist
             heap.clear()
             break
-        dx = [1, -1, 0, 0]
-        dy = [0, 0, 1, -1]
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            if 0<=nx<n and 0<=ny<n and visited[nx][ny] > now[0] + board[nx][ny]:
-                visited[nx][ny] = now[0] + board[nx][ny]
-                heapq.heappush(heap,(visited[nx][ny],[nx,ny]))
+        for dx, dy in ((1, 0), (0, 1), (-1, 0), (0, -1)):
+            nx, ny = x+dx, y+dy
+            if 0<=nx<n and 0<=ny<n and visited[nx][ny] > dist + board[nx][ny]:
+                visited[nx][ny] = dist+board[nx][ny]
+                heapq.heappush(heap, (visited[nx][ny], (nx, ny)))
+    print(f'#{tc}', end=' ')
+    print(ans)
