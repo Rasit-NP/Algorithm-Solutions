6cafa43ad30f6b4e2c3c0fcd44fd748a99c6213e
RASIT <53307918+Rasit-NP@users.noreply.github.com>
2025-09-17 09:02:40 +0900

    [Bronze III] Title: 할로윈의 사탕, Time: 92 ms, Memory: 108384 KB -BaekjoonHub

---
+++ 백준/Bronze/10178. 할로윈의 사탕/할로윈의 사탕.py
@@ -0,0 +1,8 @@
+import sys
+
+t = int(sys.stdin.readline())
+
+for _ in range(t):
+    c, v = map(int, sys.stdin.readline().split())
+
+    sys.stdout.write(f'You get {c//v} piece(s) and your dad gets {c%v} piece(s).\n')
