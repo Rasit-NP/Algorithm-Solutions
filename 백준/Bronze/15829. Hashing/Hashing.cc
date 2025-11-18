e4592c1bad014cc6dcf65ac9186110206a452a64
RASIT <53307918+Rasit-NP@users.noreply.github.com>
2025-11-18 11:31:52 +0900

    [Bronze II] Title: Hashing, Time: 0 ms, Memory: 2020 KB, Score: 50 point -BaekjoonHub

---
+++ 백준/Bronze/15829. Hashing/Hashing.cc
@@ -0,0 +1,22 @@
+# include <iostream>
+using namespace std;
+
+int main(void) {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	
+	int l, i=0;
+	long long hash = 0;
+	cin >> l;
+	for (; i < l; i++) {
+		static int coef = 1;
+		char letter;
+		cin >> letter;
+		long long num = letter - 'a' + 1;
+		hash += num * coef;
+		hash %= 1234567891;
+		coef *= 31;
+		coef %= 1234567891;
+	}
+	cout << hash << '\n';
+}
