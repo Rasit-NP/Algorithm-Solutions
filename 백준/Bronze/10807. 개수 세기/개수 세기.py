34e0f86ec505bf82623940c5ceb12610a1d09161
RASIT <53307918+Rasit-NP@users.noreply.github.com>
2025-11-24 17:22:09 +0900

    [Bronze V] Title: 개수 세기, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Bronze/10807. 개수 세기/개수 세기.py
+++ 백준/Bronze/10807. 개수 세기/개수 세기.py
@@ -2,12 +2,16 @@ import sys
 
 n = int(sys.stdin.readline())
 
-nums = list(map(int, sys.stdin.readline().split()))
+lst = list(map(int, sys.stdin.readline().split()))
 
-v = int(sys.stdin.readline())
-count = 0
-for num in nums:
-    if num == v:
-        count += 1
+forv = int(sys.stdin.readline())
 
-print(count)
+
+cnt = 0
+for x in lst:
+    if x == forv:
+        cnt += 1
+
+
+
+print(cnt)
