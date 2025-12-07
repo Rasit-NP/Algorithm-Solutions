87e138212da3b818b1267f0b6701da0ed143ee08
RASIT <53307918+Rasit-NP@users.noreply.github.com>
2025-12-07 23:08:53 +0900

    [Bronze V] Title: Rook, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2393. Rook/Rook.cc
@@ -0,0 +1,16 @@
+# include <iostream>
+using namespace std;
+
+int main(void){
+    cout << "  ___  ___  ___\n";
+    cout << "  | |__| |__| |\n";
+    cout << "  |           |\n";
+    cout << "   \\_________/\n";
+    cout << "    \\_______/\n";
+    for (int i=0; i<4; i++)
+        cout << "     |     |\n";
+    cout << "     |_____|\n";
+    cout << "  __/       \\__\n";
+    cout << " /             \\\n";
+    cout << "/_______________\\\n";
+}
