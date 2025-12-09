906d64040ac6eba6737ad9f625a06623b117af2c
RASIT <53307918+Rasit-NP@users.noreply.github.com>
2025-12-09 15:23:26 +0900

    [Bronze V] Title: M-Climb, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/34411. M－Climb/M－Climb.cc
@@ -0,0 +1,11 @@
+# include <iostream>
+using namespace std;
+
+int main(void){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+    int m, n, y;
+    cin >> m >> n >> y;
+    cout << m*n << '\n';
+}
