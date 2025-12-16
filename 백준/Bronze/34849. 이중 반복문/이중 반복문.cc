f3128b0d81f7c16469ffbc277aac3c43423e7b3f
RASIT <53307918+Rasit-NP@users.noreply.github.com>
2025-12-16 23:48:36 +0900

    [Bronze V] Title: 이중 반복문, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/34849. 이중 반복문/이중 반복문.cc
@@ -0,0 +1,9 @@
+# include <iostream>
+using namespace std;
+
+int main(void){
+    int n; cin >> n;
+    
+    if (n > 10000)    cout << "Time limit exceeded\n";
+    else              cout << "Accepted\n";
+}
