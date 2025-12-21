41b6fac54717c6269e9c6ca5f2dd2b21ba7ea4ef
RASIT <53307918+Rasit-NP@users.noreply.github.com>
2025-12-21 15:55:49 +0900

    [Bronze V] Title: 徒競走 (Footrace), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/33165. 徒競走 （Footrace）/徒競走 （Footrace）.cc
@@ -0,0 +1,10 @@
+# include <iostream>
+using namespace std;
+
+int main(void){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+    int t, v; cin >> t >> v;
+    cout << t*v << '\n';
+}
