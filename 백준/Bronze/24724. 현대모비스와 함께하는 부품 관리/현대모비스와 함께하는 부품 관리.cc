deb2a16722e689df31e89e1b9ac74d40ecac368b
RASIT <53307918+Rasit-NP@users.noreply.github.com>
2026-01-07 09:08:34 +0900

    [Bronze IV] Title: 현대모비스와 함께하는 부품 관리, Time: 20 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24724. 현대모비스와 함께하는 부품 관리/현대모비스와 함께하는 부품 관리.cc
@@ -0,0 +1,24 @@
+# include <iostream>
+using namespace std;
+
+void solve(){
+    int n;  cin >> n;
+    int a, b;   cin >> a >> b;
+
+    for (int i=0; i<n; i++){
+        int u, v;   cin >> u >> v;
+    }
+}
+
+int main(void){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);  cout.tie(NULL);
+
+    int T;  cin >> T;
+    int t = 0;
+    while (t++ < T){
+        cout << "Material Management " << t << '\n';
+        solve();
+        cout << "Classification ---- End!\n";
+    }
+}
