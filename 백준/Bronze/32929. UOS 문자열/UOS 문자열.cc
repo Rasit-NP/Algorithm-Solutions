09c184dbc48f2f5ed01baad8e14723984eef5b4d
RASIT <53307918+Rasit-NP@users.noreply.github.com>
2026-02-02 20:11:07 +0900

    [Bronze V] Title: UOS 문자열, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/32929. UOS 문자열/UOS 문자열.cc
@@ -0,0 +1,20 @@
+# include <iostream>
+using namespace std;
+
+int main(void){
+    int x;  cin >> x;
+    switch(x%3){
+        case 0:{
+            cout << "S";
+            break;
+        }
+        case 1:{
+            cout << "U";
+            break;
+        }
+        case 2:{
+            cout << "O";
+            break;
+        }
+    }
+}
