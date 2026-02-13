b22ea1a3197286aafae31f183467776c77464c2d
RASIT <53307918+Rasit-NP@users.noreply.github.com>
2026-02-13 11:39:44 +0900

    [Bronze V] Title: 정보섬의 대중교통, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/28113. 정보섬의 대중교통/정보섬의 대중교통.cc
@@ -0,0 +1,16 @@
+# include <iostream>
+using namespace std;
+
+int main(void){
+    int n, a, b;    cin >> n >> a >> b;
+
+    if (a < b){
+        cout << "Bus";
+    }
+    else if (a > b){
+        cout << "Subway";
+    }
+    else {
+        cout << "Anything";
+    }
+}
