9aff0d9df5ab5d2ed6f0bcd0ba488e5f32d041a0
RASIT <53307918+Rasit-NP@users.noreply.github.com>
2026-02-14 18:43:02 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.cc
@@ -0,0 +1,11 @@
+# include <iostream>
+# include <string>
+using namespace std;
+
+int main(void){
+    string word;    cin >> word;
+
+    cout << ":fan::fan::fan:\n";
+    cout << ":fan::" << word << "::fan:\n";
+    cout << ":fan::fan::fan:\n";
+}
