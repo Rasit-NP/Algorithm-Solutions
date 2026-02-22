c3a40dfa9bfe8076459dd93ad0d3dcd3a6028e1a
RASIT <53307918+Rasit-NP@users.noreply.github.com>
2026-02-22 23:40:58 +0900

    [Bronze III] Title: 캥거루 세마리2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11034. 캥거루 세마리2/캥거루 세마리2.cc
@@ -0,0 +1,10 @@
+# include <iostream>
+# include <algorithm>
+using namespace std;
+
+int main(void){
+    int a, b, c;
+    while(cin >> a >> b >> c){
+        cout << max(b-a, c-b) - 1 << endl;
+    }
+}
