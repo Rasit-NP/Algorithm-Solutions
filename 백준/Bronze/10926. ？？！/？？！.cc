f7faf7607a15d6ba32950330e9dd6a39eaa87250
RASIT <53307918+Rasit-NP@users.noreply.github.com>
2026-02-27 14:11:33 +0900

    [Bronze V] Title: ??!, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.cc
@@ -0,0 +1,11 @@
+# include <iostream>
+# include <string>
+using namespace std;
+
+int main(void){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);  cout.tie(NULL);
+
+    string word;    cin >> word;
+    cout << word << "??!";
+}
