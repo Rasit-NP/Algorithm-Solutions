1ae895e9aceaed790c24eb5cf159c8a988f94c84
RASIT <53307918+Rasit-NP@users.noreply.github.com>
2026-03-01 17:14:35 +0900

    [Bronze V] Title: 자동완성, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24883. 자동완성/자동완성.cc
@@ -0,0 +1,12 @@
+# include <iostream>
+using namespace std;
+
+int main(void){
+    char let;    cin >> let;
+    if (let == 'N' || let == 'n'){
+        cout << "Naver D2";
+    }
+    else {
+        cout << "Naver Whale";
+    }
+}
