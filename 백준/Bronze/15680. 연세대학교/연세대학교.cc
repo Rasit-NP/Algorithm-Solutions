403729a19f8e083e95b43f530800653116207c90
RASIT <53307918+Rasit-NP@users.noreply.github.com>
2026-03-08 12:38:05 +0900

    [Bronze V] Title: 연세대학교, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15680. 연세대학교/연세대학교.cc
@@ -0,0 +1,12 @@
+# include <iostream>
+using namespace std;
+
+int main(void){
+    int n;    cin >> n;
+    if (n){
+        cout << "Leading the Way to the Future";
+    }
+    else {
+        cout << "YONSEI";
+    }
+}
