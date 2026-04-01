7c3b916f1293fb19e27c7eab0b6ae3630ff5c91c
RASIT <53307918+Rasit-NP@users.noreply.github.com>
2026-04-01 15:37:28 +0900

    [Gold I] Title: 레몬 경로, Time: 72 ms, Memory: 13608 KB, Score: 6 point -BaekjoonHub

---
+++ 백준/Gold/34252. 레몬 경로/레몬 경로.cc
@@ -0,0 +1,97 @@
+# include <iostream>
+# include <vector>
+# include <queue>
+# include <climits>
+# define MAX 998'244'353
+using namespace std;
+
+vector<vector<pair<long long, long long>>> edges;
+vector<int> visited;
+queue<int> q;
+vector<pair<int, int>> distances;
+
+int gcd(int a, int b){
+    if (a < b){
+        swap(a, b);
+    }
+    int tmp;
+    while (a%b){
+        tmp = a%b;
+        a = b;
+        b = tmp;
+    }
+    return b;
+}
+
+long long pow(long long a, int x){
+    long long res = 1;
+    a %= MAX;
+    long long bias = a;
+    while(x){
+        if (x&1){
+            res *= bias;
+        }
+        bias *= bias;
+
+        res %= MAX;
+        bias %= MAX;
+
+        x >>= 1;
+    }
+    return res;
+}
+
+long long restruct(pair<int, int>& input){
+    input.first %= MAX;
+    auto [dist, cnt] = input;
+    int g;
+    if (dist == 0 || cnt == 0){
+        g = 1;
+    }
+    else{
+        g = gcd(dist, cnt);
+    }
+    dist /= g;
+    cnt /= g;
+
+    return (dist * pow(cnt, MAX-2)) % MAX;
+}
+
+int main(void){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);  cout.tie(NULL);
+
+    int n, m;   cin >> n >> m;
+    edges.assign(n+1, vector<pair<long long, long long>>(0));
+    visited.assign(n+1, INT_MAX);
+    distances.assign(n+1, {0, 0});
+
+    for (int i=0; i<m; i++){
+        int u, v, c;    cin >> u >> v >> c;
+        edges[u].push_back({v, c});
+        edges[v].push_back({u, c});
+    }
+
+    q.push(1);
+    distances[1] = {0, 1};
+    visited[1] = 0;
+
+    while (q.size()){
+        int x = q.front();  q.pop();
+        for (const auto& [nx, c] : edges[x]){
+            if (visited[x] + 1 > visited[nx]){
+                continue;
+            }
+            distances[nx].first += (distances[x].first + c) * distances[x].second;
+            distances[nx].second += distances[x].second;
+            if (visited[nx] > visited[x] + 1){
+                visited[nx] = visited[x] + 1;
+                q.push(nx);
+            }
+        }
+    }
+
+    for (int i=2; i<=n; i++){
+        cout << restruct(distances[i]) << '\n';
+    }
+}
