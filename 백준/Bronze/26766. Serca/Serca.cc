5ef19328636f35019074158847bc7f8753eccb5c
RASIT <53307918+Rasit-NP@users.noreply.github.com>
2026-04-06 09:48:54 +0900

    [Bronze V] Title: Serca, Time: 76 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/26766. Serca/Serca.cc
@@ -0,0 +1,18 @@
+# include <iostream>
+using namespace std;
+
+int main(void){
+    int n;    cin >> n;
+    while (n--){
+        cout << "\
+ @@@   @@@ \n\
+@   @ @   @\n\
+@    @    @\n\
+@         @\n\
+ @       @ \n\
+  @     @  \n\
+   @   @   \n\
+    @ @    \n\
+     @     \n";
+    }
+}
