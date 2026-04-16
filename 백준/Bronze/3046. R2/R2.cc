3cd52af6296d8c9df5cf5d061e1274a6b792ab1c
RASIT <53307918+Rasit-NP@users.noreply.github.com>
2026-04-16 14:59:05 +0900

    [Bronze IV] Title: R2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/3046. R2/R2.cc
@@ -0,0 +1,11 @@
+# include <iostream>
+using namespace std;
+
+int main(void){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);  cout.tie(NULL);
+
+    int r, s;   cin >> r >> s;
+
+    cout << 2*s - r;
+}
