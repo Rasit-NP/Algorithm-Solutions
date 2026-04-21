8cc0495133fe6052ff13a9f0e40558d17e3fab17
RASIT <53307918+Rasit-NP@users.noreply.github.com>
2026-04-21 09:57:15 +0900

    [Bronze IV] Title: 세 수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10817. 세 수/세 수.cc
@@ -0,0 +1,15 @@
+# include <iostream>
+# include <algorithm>
+using namespace std;
+
+int main(void){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);  cout.tie(NULL);
+
+    int a[3];
+    for (int i=0; i<3; i++){
+        cin >> a[i];
+    }
+    sort(a, a+3);
+    cout << a[1];
+}
