04085f94199779f73bdd805a94ab11c8cff02761
RASIT <53307918+Rasit-NP@users.noreply.github.com>
2026-04-24 15:01:44 +0900

    [Bronze IV] Title: 심부름 가는 길, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5554. 심부름 가는 길/심부름 가는 길.cc
@@ -0,0 +1,11 @@
+# include <iostream>
+using namespace std;
+
+int main(void){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);  cout.tie(NULL);
+
+    int a, b, c, d;     cin >> a >> b >> c >> d;
+    int sum = a+b+c+d;
+    cout << sum/60 << '\n' << sum%60;
+}
