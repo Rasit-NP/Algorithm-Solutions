6bab01040a3ea8cba4fbd7592a2f6504ee2ef6e4
RASIT <53307918+Rasit-NP@users.noreply.github.com>
2026-04-28 09:00:20 +0900

    [Bronze IV] Title: MT 준비, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/34891. MT 준비/MT 준비.cc
@@ -0,0 +1,10 @@
+# include <iostream>
+using namespace std;
+
+int main(void){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);  cout.tie(NULL);
+
+    int n, m;   cin >> n >> m;
+    cout << (n+m-1)/m;
+}
