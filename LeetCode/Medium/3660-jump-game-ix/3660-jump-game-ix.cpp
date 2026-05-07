94b7f2b4ac0bed349e85ba3377eadb20986c4b84
RASIT <53307918+Rasit-NP@users.noreply.github.com>
2026-05-08 01:24:43 +0900

    [Medium] Title: 3660-jump-game-ix, Time: 12 ms (60.64%) , Memory: 219.4 MB (82.98%) -LeetHub

---
+++ LeetCode/Medium/3660-jump-game-ix/3660-jump-game-ix.cpp
@@ -0,0 +1,30 @@
+#include <vector>
+#include <algorithm>
+#include <climits>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> maxValue(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> ans(n);
+        vector<int> preMax(n);
+
+        preMax[0] = nums[0];
+        for (int i=1; i<n; i++) {
+            preMax[i] = max(preMax[i - 1], nums[i]);
+        }
+        
+        int sufMin = INT_MAX;
+        for (int i=n-1; i>= 0; i--) {
+            if (i<n-1 && preMax[i] > sufMin) {
+                ans[i] = ans[i + 1];
+            } else {
+                ans[i] = preMax[i];
+            }
+            sufMin = min(sufMin, nums[i]);
+        }
+        
+        return ans;
+    }
+};
