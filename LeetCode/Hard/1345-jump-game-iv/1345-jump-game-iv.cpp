0988c75fb33c7ee1d10f601854ce337b3958d283
RASIT <53307918+Rasit-NP@users.noreply.github.com>
2026-05-18 10:48:46 +0900

    [Hard] Title: 1345-jump-game-iv , Time: 131 ms (37.62%), Memory: 83.6 MB (39.37%)  -LeetHub

---
+++ LeetCode/Hard/1345-jump-game-iv/1345-jump-game-iv.cpp
@@ -0,0 +1,55 @@
+# include <vector>
+# include <queue>
+# include <map>
+# include <set>
+using namespace std;
+
+class Solution {
+private:
+public:
+    int minJumps(vector<int>& arr) {
+        int n = arr.size();
+        map<int, vector<int>> edges;
+        vector<int> visited(n, -1);
+        queue<int> q;
+        set<int> passed;
+
+        for (int i=0; i<n; i++){
+            int num = arr[i];
+            edges[num].push_back(i);
+        }
+
+        q.push(0);
+        visited[0] = 0;
+
+        while (q.size()){
+            int x = q.front();  q.pop();
+            if (x == n-1){
+                return visited[x];
+            }
+
+            int val = arr[x];
+            if (passed.find(val) == passed.end()){
+                for (int nx : edges[val]){
+                    if (visited[nx] == -1){
+                        visited[nx] = visited[x] + 1;
+                        q.push(nx);
+                    }
+                }
+                passed.insert(val);
+            }
+
+            for (int dx : {-1, 1}){
+                int nx = x + dx;
+                if (nx < 0 || nx >= n){
+                    continue;
+                }
+                if (visited[nx] == -1){
+                    visited[nx] = visited[x] + 1;
+                    q.push(nx);
+                }
+            }
+        }
+        return visited[n-1];
+    }
+};
