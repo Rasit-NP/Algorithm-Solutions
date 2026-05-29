7eb6b379b636eec6b231289e21931b747bc8d7e3
RASIT <53307918+Rasit-NP@users.noreply.github.com>
2026-05-29 10:20:36 +0900

    [Medium] Title: 0015-3sum , Time: 61 ms (17.97%), Memory: 29 MB (72%)  -LeetHub

--- LeetCode/Medium/0015-3sum/0015-3sum.cpp
+++ LeetCode/Medium/0015-3sum/0015-3sum.cpp
@@ -6,11 +6,13 @@ class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
         vector<vector<int>> res;
-        set<vector<int>> sets;
         int n = nums.size();
         sort(nums.begin(), nums.end());
 
         for (int i=0; i<n; i++){
+            if (i > 0 && nums[i] == nums[i-1]){
+                continue;
+            }
             int start = nums[i];
             int l = i+1, r = n-1;
             while (l < r){
@@ -22,11 +24,15 @@ public:
                     --r;
                 }
                 else {
-                    if (sets.find({start, nums[l], nums[r]}) == sets.end()){
-                        res.push_back({start, nums[l], nums[r]});
-                        sets.insert({start, nums[l], nums[r]});
+                    res.push_back({start, nums[l], nums[r]});
+                    while (l < r && nums[l] == nums[l+1]){
+                        ++l;
+                    }
+                    while (l < r && nums[r] == nums[r-1]){
+                        --r;
                     }
                     ++l;
+                    --r;
                 }
             }
         }
