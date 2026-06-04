5876296d8f2777d1241f9639753c351468bb2680
RASIT <53307918+Rasit-NP@users.noreply.github.com>
2026-06-04 15:31:42 +0900

    [Easy] Title: 1929-concatenation-of-array , Time: 2 ms (16.12%), Memory: 16.9 MB (58.06%)  -LeetHub

---
+++ LeetCode/Easy/1929-concatenation-of-array/1929-concatenation-of-array.cpp
@@ -0,0 +1,16 @@
+# include <vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> getConcatenation(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> res(2*n, 0);
+        for (int i=0; i<n; i++){
+            res[i] = nums[i];
+            res[i+n] = nums[i];
+        }
+
+        return res;
+    }
+};
