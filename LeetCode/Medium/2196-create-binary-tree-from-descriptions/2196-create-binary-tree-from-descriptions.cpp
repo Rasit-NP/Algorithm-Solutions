7a5b75ce926fdfe1c6df320932a29402120b266d
RASIT <53307918+Rasit-NP@users.noreply.github.com>
2026-06-07 22:00:14 +0900

    [Medium] Title: 2196-create-binary-tree-from-descriptions, Time: 242 ms (30.62%) , Memory: 320.6 MB (5.27%) -LeetHub

--- LeetCode/Medium/2196-create-binary-tree-from-descriptions/2196-create-binary-tree-from-descriptions.cpp
+++ LeetCode/Medium/2196-create-binary-tree-from-descriptions/2196-create-binary-tree-from-descriptions.cpp
@@ -1,20 +1,20 @@
-# include <vector>
+# include <unordered_map>
 using namespace std;
 
 class Solution {
 private:
-    vector<int> parents;
-    vector<pair<int, int>> childs;
+    unordered_map<int, int> parents;
+    unordered_map<int, pair<int, int>> childs;
 
     void dfs(TreeNode* now){
         if (now->left != nullptr){
             int val = now->left->val;
             int left = childs[val].first, right = childs[val].second;
 
-            if (left != -1){
+            if (left != 0){
                 now->left->left = new TreeNode(left);
             }
-            if (right != -1){
+            if (right != 0){
                 now->left->right = new TreeNode(right);
             }
             dfs(now->left);
@@ -23,10 +23,10 @@ private:
             int val = now->right->val;
             int left = childs[val].first, right = childs[val].second;
 
-            if (left != -1){
+            if (left != 0){
                 now->right->left = new TreeNode(left);
             }
-            if (right != -1){
+            if (right != 0){
                 now->right->right = new TreeNode(right);
             }
             dfs(now->right);
@@ -35,8 +35,6 @@ private:
 
 public:
     TreeNode* createBinaryTree(vector<vector<int>>& descriptions) {
-        parents.assign(100001, -1);
-        childs.assign(100001, {-1, -1});
         for (vector<int>& description : descriptions){
             int x = description[1];
             int parent = description[0];
@@ -49,11 +47,11 @@ public:
             }
         }
         int rootVal = descriptions[0][0];
-        while (parents[rootVal] != -1){
+        while (parents[rootVal] != 0){
             rootVal = parents[rootVal];
         }
 
-        TreeNode* root = new TreeNode(rootVal, (childs[rootVal].first != -1 ? new TreeNode(childs[rootVal].first) : nullptr), (childs[rootVal].second != -1 ? new TreeNode(childs[rootVal].second) : nullptr));
+        TreeNode* root = new TreeNode(rootVal, (childs[rootVal].first != 0 ? new TreeNode(childs[rootVal].first) : nullptr), (childs[rootVal].second != 0 ? new TreeNode(childs[rootVal].second) : nullptr));
 
         dfs(root);
 
