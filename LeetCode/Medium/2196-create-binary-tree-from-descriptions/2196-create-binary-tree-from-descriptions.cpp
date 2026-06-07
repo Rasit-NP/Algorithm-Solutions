49ebef0f3c29840887a6099c23ebfa8f4cc38abc
RASIT <53307918+Rasit-NP@users.noreply.github.com>
2026-06-07 21:56:45 +0900

    [Medium] Title: 2196-create-binary-tree-from-descriptions, Time: 97 ms (93.3%) , Memory: 348.5 MB (5.12%) -LeetHub

---
+++ LeetCode/Medium/2196-create-binary-tree-from-descriptions/2196-create-binary-tree-from-descriptions.cpp
@@ -0,0 +1,73 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+# include <vector>
+using namespace std;
+
+class Solution {
+private:
+    vector<int> parents;
+    vector<pair<int, int>> childs;
+
+    void dfs(TreeNode* now){
+        if (now->left != nullptr){
+            int val = now->left->val;
+            int left = childs[val].first, right = childs[val].second;
+
+            if (left != -1){
+                now->left->left = new TreeNode(left);
+            }
+            if (right != -1){
+                now->left->right = new TreeNode(right);
+            }
+            dfs(now->left);
+        }
+        if (now->right != nullptr){
+            int val = now->right->val;
+            int left = childs[val].first, right = childs[val].second;
+
+            if (left != -1){
+                now->right->left = new TreeNode(left);
+            }
+            if (right != -1){
+                now->right->right = new TreeNode(right);
+            }
+            dfs(now->right);
+        }
+    }
+
+public:
+    TreeNode* createBinaryTree(vector<vector<int>>& descriptions) {
+        parents.assign(100001, -1);
+        childs.assign(100001, {-1, -1});
+        for (vector<int>& description : descriptions){
+            int x = description[1];
+            int parent = description[0];
+            parents[x] = parent;
+            if (description[2]){
+                childs[parent].first = x;
+            }
+            else {
+                childs[parent].second = x;
+            }
+        }
+        int rootVal = descriptions[0][0];
+        while (parents[rootVal] != -1){
+            rootVal = parents[rootVal];
+        }
+
+        TreeNode* root = new TreeNode(rootVal, (childs[rootVal].first != -1 ? new TreeNode(childs[rootVal].first) : nullptr), (childs[rootVal].second != -1 ? new TreeNode(childs[rootVal].second) : nullptr));
+
+        dfs(root);
+
+        return root;
+    }
+};
