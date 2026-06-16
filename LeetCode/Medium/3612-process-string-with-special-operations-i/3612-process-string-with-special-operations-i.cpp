850d809ec322c6b0408e0955cfc43cfc68c6107d
RASIT <53307918+Rasit-NP@users.noreply.github.com>
2026-06-16 17:39:54 +0900

    [Medium] Title: 3612-process-string-with-special-operations-i, Time: 36 ms (5.3%) , Memory: 44.3 MB (14.02%) -LeetHub

---
+++ LeetCode/Medium/3612-process-string-with-special-operations-i/3612-process-string-with-special-operations-i.cpp
@@ -0,0 +1,31 @@
+# include <string>
+# include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    string processStr(string s) {
+        string res = "";
+        for (char let : s){
+            if (let >= 'a' && let <= 'z'){
+                res.push_back(let);
+            }
+            else if (let == '*'){
+                if (res.size()){
+                    res.pop_back();
+                }
+            }
+            else if (let == '#'){
+                int len = res.size();
+                for (int i=0; i<len; i++){
+                    res.push_back(res[i]);
+                }
+            }
+            else if (let == '%'){
+                reverse(res.begin(), res.end());
+            }
+        }
+
+        return res;
+    }
+};
