68b35c9a21ccb142771d72e2af43f74a80718b01
RASIT <53307918+Rasit-NP@users.noreply.github.com>
2026-06-27 15:50:39 +0900

    [Medium] Title: 3020-find-the-maximum-number-of-elements-in-subset, Time: 131 ms (34.29%), Memory: 135.4 MB (10.86%) -LeetHub

--- LeetCode/Medium/3020-find-the-maximum-number-of-elements-in-subset/3020-find-the-maximum-number-of-elements-in-subset.cpp
+++ LeetCode/Medium/3020-find-the-maximum-number-of-elements-in-subset/3020-find-the-maximum-number-of-elements-in-subset.cpp
@@ -6,35 +6,45 @@ using namespace std;
 
 class Solution {
 private:
+    int res = 0;
     int sq = sqrt(2'000'000'000);
     unordered_map<int, int> count;
     unordered_map<int, int> dp;
+
+    int calDp(int num){
+        if (count.find(num) == count.end()){
+            return 0;
+        }
+        if (dp[num]){
+            return dp[num];
+        }
+
+        if (num >= sq){
+            dp[num] = 1;
+        }
+        else if (num == 1){
+            dp[1] = count[1] - !(count[1]&1);
+        }
+        else {
+            if (count[num] >= 2 && calDp(num*num)){
+                dp[num] = dp[num*num] + 2;
+            }
+            else {
+                dp[num] = 1;
+            }
+        }
+        return dp[num];
+    }
 public:
     int maximumLength(vector<int>& nums) {
         int n = nums.size();
-        sort(nums.begin(), nums.end());
 
-        int res = upper_bound(nums.begin(), nums.end(), 1) - lower_bound(nums.begin(), nums.end(), 1);
-        res = res - !(res&1);
-        
-        for (int i=n-1; i>=0; --i){
-            int num = nums[i];
+        for (int num : nums){
             count[num]++;
-            if (num > sq){
-                dp[num] = 1;
-            }
-            else if (num == 1){
-                dp[num] = 1;
-            }
-            else {
-                if (count[num] >= 2 && dp[num*num]){
-                    dp[num] = dp[num*num] + 2;
-                }
-                else {
-                    dp[num] = 1;
-                }
-            }
-            res = max(res, dp[num]);
+        }
+
+        for (const auto& [num, val] : count){
+            res = max(res, calDp(num));
         }
 
         return res;
