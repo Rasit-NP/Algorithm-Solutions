697b44ce415f3f7ef6962f73b111049ba76e16e2
RASIT <53307918+Rasit-NP@users.noreply.github.com>
2026-07-06 07:27:06 +0900

    [Easy] Title: 0607-sales-person, Time: 435 ms (11.74%) , Memory: 0B (100%) -LeetHub

---
+++ LeetCode/Easy/0607-sales-person/0607-sales-person.sql
@@ -0,0 +1,10 @@
+select "name"
+from SalesPerson as s
+where s.sales_id not in 
+(
+    select o.sales_id
+    from Orders as o
+    left join Company as c
+    on c.com_id = o.com_id
+    where c.name = 'RED'
+);
