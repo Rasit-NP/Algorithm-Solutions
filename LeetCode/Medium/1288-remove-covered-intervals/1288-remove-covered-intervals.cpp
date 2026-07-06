b62f5acf409f1ced9df1d53a1bfd51b8f5df654b
RASIT <53307918+Rasit-NP@users.noreply.github.com>
2026-07-06 12:59:28 +0900

    [Medium] Title: 1288-remove-covered-intervals, Time: 0 ms (100%), Memory: 15.2 MB (30.6%) -LeetHub

--- LeetCode/Medium/1288-remove-covered-intervals/1288-remove-covered-intervals.cpp
+++ LeetCode/Medium/1288-remove-covered-intervals/1288-remove-covered-intervals.cpp
@@ -3,31 +3,27 @@
 using namespace std;
 
 bool compare(vector<int>& a, vector<int>& b){
-    if (a[0] < b[0]){
-        return true;
+    if (a[0] == b[0]){
+        return a[1] > b[1];
     }
-    else if (a[0] == b[0] && a[1] > b[1]){
-        return true;
-    }
-    else {
-        return false;
-    }
+    return a[0] < b[0];
 }
 
 class Solution {
 public:
     int removeCoveredIntervals(vector<vector<int>>& intervals) {
         int n = intervals.size();
-        int res = n;
-        vector<int> isDeleted(n, 0);
         sort(intervals.begin(), intervals.end(), compare);
 
-        for (int i=0; i<n; i++){
-            for (int j=i+1; j<n; j++){
-                if (intervals[i][0] <= intervals[j][0] && intervals[i][1] >= intervals[j][1] && !isDeleted[j]){
-                    --res;
-                    isDeleted[j] = 1;
-                }
+        int res = n;
+        int maxR = 0;
+
+        for (vector<int>& interval : intervals){
+            if (interval[1] <= maxR){
+                --res;
+            }
+            else {
+                maxR = interval[1];
             }
         }
 
