bd435a8f759754cf1604a9e9387b438b1619ec15
RASIT <53307918+Rasit-NP@users.noreply.github.com>
2026-07-06 13:01:15 +0900

    [Medium] Title: 1288-remove-covered-intervals, Time: 0 ms (100%), Memory: 15 MB (82.12%) -LeetHub

--- LeetCode/Medium/1288-remove-covered-intervals/1288-remove-covered-intervals.cpp
+++ LeetCode/Medium/1288-remove-covered-intervals/1288-remove-covered-intervals.cpp
@@ -2,18 +2,17 @@
 # include <algorithm>
 using namespace std;
 
-bool compare(vector<int>& a, vector<int>& b){
-    if (a[0] == b[0]){
-        return a[1] > b[1];
-    }
-    return a[0] < b[0];
-}
 
 class Solution {
 public:
     int removeCoveredIntervals(vector<vector<int>>& intervals) {
         int n = intervals.size();
-        sort(intervals.begin(), intervals.end(), compare);
+        sort(intervals.begin(), intervals.end(), [](vector<int>& a, vector<int>& b){
+            if (a[0] == b[0]){
+                return a[1] > b[1];
+            }
+            return a[0] < b[0];
+        });
 
         int res = n;
         int maxR = 0;
