968ebdcb3ce27e2212b89fe393dcdf7b9b32157d
RASIT <53307918+Rasit-NP@users.noreply.github.com>
2026-07-09 15:08:20 +0900

    [Medium] Title: 3532-path-existence-queries-in-a-graph-i, Time: 4 ms (93.7%), Memory: 224.2 MB (66.03%) -LeetHub

--- LeetCode/Medium/3532-path-existence-queries-in-a-graph-i/3532-path-existence-queries-in-a-graph-i.cpp
+++ LeetCode/Medium/3532-path-existence-queries-in-a-graph-i/3532-path-existence-queries-in-a-graph-i.cpp
@@ -4,56 +4,25 @@ using namespace std;
 
 class Solution {
 private:
-    int n;
     vector<bool> res;
-    vector<int> maxEdge;
-    vector<int> range;
+    vector<int> group;
 
-    int getRange(int x){
-        if (range[x] != -1){
-            return range[x];
-        }
-
-        if (maxEdge[x] == x){
-            range[x] = x;
-            return x;
-        }
-
-        int val = getRange(maxEdge[x]);
-        range[x] = val;
-        return val;
-    }
-
-    bool solve(int u, int v){
-        int a = min(u, v), b = max(u, v);
-
-        if (b <= range[a]){
-            return true;
-        }
-        else {
-            return false;
-        }
-    }
 public:
     vector<bool> pathExistenceQueries(int n, vector<int>& nums, int maxDiff, vector<vector<int>>& queries) {
-        this->n = n;
-        maxEdge.assign(n, 0);
-        range.assign(n, -1);
         res.reserve(queries.size());
+        group.assign(n, 0);
 
-        for (int l=0, r=0; l<n; l++){
-            while (r < n-1 && nums[r+1]-nums[l] <= maxDiff){
-                ++r;
+        for (int i=1; i<n; i++){
+            if (nums[i] - nums[i-1] <= maxDiff){
+                group[i] = group[i-1];
             }
-            maxEdge[l] = r;
-        }
-
-        for (int i=0; i<n; i++){
-            range[i] = getRange(i);
+            else {
+                group[i] = group[i-1] + 1;
+            }
         }
 
         for (const auto& q : queries){
-            res.emplace_back(solve(q[0], q[1]));
+            res.emplace_back(group[q[0]] == group[q[1]]);
         }
 
         return res;
