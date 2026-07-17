a921165f393d7c88e9608e056adde921ae989a2a
RASIT <53307918+Rasit-NP@users.noreply.github.com>
2026-07-17 23:11:30 +0900

    [Hard] Title: 3312-sorted-gcd-pair-queries, Time: 32 ms (92%) , Memory: 127.3 MB (73.33%) -LeetHub

--- LeetCode/Hard/3312-sorted-gcd-pair-queries/3312-sorted-gcd-pair-queries.cpp
+++ LeetCode/Hard/3312-sorted-gcd-pair-queries/3312-sorted-gcd-pair-queries.cpp
@@ -11,29 +11,32 @@ private:
     vector<int> res;
 public:
     vector<int> gcdValues(vector<int>& nums, vector<llong>& queries) {
+        int MAX = *max_element(nums.begin(), nums.end());
+        
         res.reserve(queries.size());
-        freq.assign(50'001, 0);
-        cnt.assign(50'001, 0);
-        gcdCount.assign(50'001, 0);
+        freq.assign(MAX+1, 0);
+        cnt.assign(MAX+1, 0);
+        gcdCount.assign(MAX+1, 0);
+
 
         for (int num : nums){
             ++freq[num];
         }
 
-        for (int i=1; i<=50'000; ++i){
-            for (int d=i; d<=50'000; d+=i){
+        for (int i=1; i<=MAX; ++i){
+            for (int d=i; d<=MAX; d+=i){
                 cnt[i] += freq[d];
             }
         }
 
-        for (int i=50'000; i>=1; --i){
+        for (int i=MAX; i>=1; --i){
             gcdCount[i] = (llong)cnt[i] * (cnt[i] - 1) / 2;
-            for (int d=2*i; d<=50'000; d+=i){
+            for (int d=2*i; d<=MAX; d+=i){
                 gcdCount[i] -= gcdCount[d];
             }
         }
 
-        for (int i=0; i<50'000; ++i){
+        for (int i=0; i<MAX; ++i){
             gcdCount[i+1] += gcdCount[i];
         }
 
