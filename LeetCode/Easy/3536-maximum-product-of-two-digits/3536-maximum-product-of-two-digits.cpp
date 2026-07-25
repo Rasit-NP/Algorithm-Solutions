8b207039514bb7018e7a9300c8b6c763c95c5b82
RASIT <53307918+Rasit-NP@users.noreply.github.com>
2026-07-25 14:00:20 +0900

    [Easy] Title: 3536-maximum-product-of-two-digits, Time: 0 ms (100%) , Memory: 9.3 MB (10.11%) -LeetHub

---
+++ LeetCode/Easy/3536-maximum-product-of-two-digits/3536-maximum-product-of-two-digits.cpp
@@ -0,0 +1,14 @@
+# include <string>
+# include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    int maxProduct(int n) {
+        string s = to_string(n);
+        sort(s.begin(), s.end());
+        int len = s.size();
+
+        return (s[len-1] - '0') * (s[len-2] - '0');
+    }
+};
