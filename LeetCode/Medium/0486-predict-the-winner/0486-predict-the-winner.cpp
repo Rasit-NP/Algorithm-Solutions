610141d3f1f4a540abd8b84cb427b0edbe676e6e
RASIT <53307918+Rasit-NP@users.noreply.github.com>
2026-08-01 23:02:37 +0900

    [Medium] Title: 0486-predict-the-winner, Time: 1253 ms (5.37%), Memory: 10.9 MB (7.85%) -LeetHub

---
+++ LeetCode/Medium/0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -0,0 +1,65 @@
+# include <vector>
+# include <deque>
+using namespace std;
+
+class Solution {
+private:
+    int n;
+    bool myTurn;
+    int scores[2] = {0, 0};
+    deque<int> dq;
+    vector<int> nums;
+
+    bool foo(int state){
+        if (state == (1<<n) - 1){
+            if (scores[1] >= scores[0])
+                return true;
+            else
+                return false;
+        }
+        int idx[2] = {dq.front(), dq.back()};
+        bool res[2];
+        for (int i=0; i<2; i++){
+            if (i)
+                dq.pop_back();
+            else
+                dq.pop_front();
+            int score = nums[idx[i]];
+            
+            scores[myTurn] += score;
+            myTurn ^= 1;
+            state ^= (1 << idx[i]);
+
+            res[i] = foo(state);
+
+            state ^= (1 << idx[i]);
+            myTurn ^= 1;
+            scores[myTurn] -= score;
+
+            if (i)
+                dq.push_back(idx[i]);
+            else 
+                dq.push_front(idx[i]);
+        }
+        if (myTurn){
+            return res[0] | res[1];
+        }
+        else {
+            return res[0] && res[1];
+        }
+    }
+public:
+    bool predictTheWinner(vector<int>& nums) {
+        n = nums.size();
+        this->nums = nums;
+
+        for (int i=0; i<n; i++){
+            dq.emplace_back(i);
+        }
+
+        myTurn = true;
+        bool res = foo(0);
+        
+        return res;
+    }
+};
