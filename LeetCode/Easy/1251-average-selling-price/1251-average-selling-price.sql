a88021b1047140e93fcbb4bb7554373656450526
RASIT <53307918+Rasit-NP@users.noreply.github.com>
2026-08-03 19:33:27 +0900

    [Easy] Title: 1251-average-selling-price, Time: 225 ms (35.16%) , Memory: 0B (100%) -LeetHub

---
+++ LeetCode/Easy/1251-average-selling-price/1251-average-selling-price.sql
@@ -0,0 +1,5 @@
+select p.product_id, coalesce(round(sum(p.price * u.units)::numeric/sum(u.units), 2), 0) as average_price
+from Prices p
+left join UnitsSold u
+on p.product_id = u.product_id and p.start_date <= u.purchase_date and u.purchase_date <= p.end_date
+group by p.product_id;
