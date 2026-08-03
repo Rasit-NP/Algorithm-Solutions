2a9e8be390df73e7c55009cef7fe703c437b80a1
RASIT <53307918+Rasit-NP@users.noreply.github.com>
2026-08-03 19:11:16 +0900

    [Hard] Title: 1406-stone-game-iii, Time: 11 ms (90.58%) , Memory: 136 MB (83.46%) -LeetHub

--- LeetCode/Hard/1406-stone-game-iii/1406-stone-game-iii.cpp
+++ LeetCode/Hard/1406-stone-game-iii/1406-stone-game-iii.cpp
@@ -5,40 +5,25 @@
 using namespace std;
 
 class Solution {
-private:
-    int n;
-    vector<int> dp, nums;
-
-    int foo(int x){
-        if (x >= n)
-            return 0;
-        else if (dp[x] != -1)
-            return dp[x];
-
-        int val = INT_MIN;
-        int now = 0;
-        for (int i=1; i<=3; i++){
-            if (x+i > n)
-                break;
-            now += nums[x+i-1];
-            val = max(val, now - foo(x+i));
+public:
+    string stoneGameIII(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> dp(n+1, INT_MIN);
+        dp[n] = 0;
+        
+        for (int x=n-1; x>=0; x--){
+            int now = 0;
+            for (int i=0; i<3; i++){
+                if (x+i>=n)
+                    break;
+                now += nums[x+i];
+                dp[x] = max(dp[x], now - dp[x+i+1]);
+            }
         }
 
-        dp[x] = val;
-        return val;
-    }
-public:
-    string stoneGameIII(vector<int>& stoneValue) {
-        n = stoneValue.size();
-        nums = move(stoneValue);
-        dp.assign(n, -1);
-        dp[n-1] = nums[n-1];
-
-        int val = foo(0);
-
-        if (val > 0)
+        if (dp[0] > 0)
             return "Alice";
-        else if (val == 0)
+        else if (dp[0] == 0)
             return "Tie";
         else
             return "Bob";
