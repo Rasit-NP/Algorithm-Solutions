ac353eb355d6e8462651d96bf5634cf8ea563d92
RASIT <53307918+Rasit-NP@users.noreply.github.com>
2026-08-10 12:38:38 +0900

    [Hard] Title: 1510-stone-game-iv, Time: 10 ms (96.32%), Memory: 8.3 MB (95.51%) -LeetHub

--- LeetCode/Hard/1510-stone-game-iv/1510-stone-game-iv.cpp
+++ LeetCode/Hard/1510-stone-game-iv/1510-stone-game-iv.cpp
@@ -1,5 +1,6 @@
 # include <vector>
 # include <algorithm>
+# define N 100'000
 using namespace std;
 
 namespace {
@@ -9,19 +10,26 @@ int sq(int x){
 };
 
 class Solution {
-public:
-    bool winnerSquareGame(int n) {
-        vector<int> dp(n+1, -1);
-        dp[0] = 0;
-
-        for (int x=1; x<=n; x++){
-            for (int dx=1; x-sq(dx)>=0; dx++){
-                dp[x] = max(dp[x], dp[x-sq(dx)]^1);
-                if (dp[x])
-                    break;
+private:
+    int f(int x){
+        if (dp[x] == -1){
+            dp[0] = 0;
+            for (int x=1; x<=N; x++){
+                for (int dx=1; x-sq(dx)>=0; dx++){
+                    dp[x] = max(dp[x], dp[x-sq(dx)]^1);
+                    if (dp[x])
+                        break;
+                }
             }
         }
 
-        return dp[n];
+        return dp[x];
+    }
+public:
+    static vector<int> dp;
+    bool winnerSquareGame(int n) {
+        return f(n);
     }
 };
+
+vector<int> Solution::dp(N+1, -1);
