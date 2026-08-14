c478746dfe9748b2e6eb27996ccaa399fc109d4b
RASIT <53307918+Rasit-NP@users.noreply.github.com>
2026-08-14 16:22:11 +0900

    [Medium] Title: 0176-second-highest-salary, Time: 311 ms (35.83%), Memory: 0B (100%) -LeetHub

--- LeetCode/Medium/0176-second-highest-salary/0176-second-highest-salary.sql
+++ LeetCode/Medium/0176-second-highest-salary/0176-second-highest-salary.sql
@@ -1,3 +1,6 @@
-select max(salary) 'SecondHighestSalary'
-from Employee
-where salary < (select max(salary) from Employee);
+select (
+    select distinct salary
+    from Employee
+    order by salary desc
+    limit 1 offset 1
+) as 'SecondHighestSalary';
