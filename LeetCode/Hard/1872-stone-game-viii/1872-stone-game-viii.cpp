552c9fbecd8ea016425d625b68cf8e5a3ed1eb6f
RASIT <53307918+Rasit-NP@users.noreply.github.com>
2026-08-25 08:02:47 +0900

    [Hard] Title: 1872-stone-game-viii, Time: 103 ms (70.87%) , Memory: 91.7 MB (72.46%) -LeetHub

--- LeetCode/Hard/1872-stone-game-viii/1872-stone-game-viii.cpp
+++ LeetCode/Hard/1872-stone-game-viii/1872-stone-game-viii.cpp
@@ -6,7 +6,6 @@ class Solution {
 public:
     int stoneGameVIII(vector<int>& stones) {
         int n = stones.size();
-        vector<int> dp(n, 0);
         vector<int> sums(n, 0);
 
         sums[0] = stones[0];
@@ -14,11 +13,9 @@ public:
             sums[i] = sums[i-1] + stones[i];
         }
 
-        dp[n-1] = sums[n-1];
-        int maxi = dp[n-1];
+        int maxi = sums[n-1];
         for (int i=n-2; i>0; i--){
-            dp[i] = sums[i] - maxi;
-            maxi = max(maxi, dp[i]);
+            maxi = max(maxi, sums[i] - maxi);
         }
 
         return maxi;
