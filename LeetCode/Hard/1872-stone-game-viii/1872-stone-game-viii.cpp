f465b3ddfbe32b0127ff5aa3004cbce63dce530b
RASIT <53307918+Rasit-NP@users.noreply.github.com>
2026-08-25 08:01:34 +0900

    [Hard] Title: 1872-stone-game-viii, Time: 107 ms (58.26%) , Memory: 95.4 MB (43.91%) -LeetHub

--- LeetCode/Hard/1872-stone-game-viii/1872-stone-game-viii.cpp
+++ LeetCode/Hard/1872-stone-game-viii/1872-stone-game-viii.cpp
@@ -16,11 +16,11 @@ public:
 
         dp[n-1] = sums[n-1];
         int maxi = dp[n-1];
-        for (int i=n-2; i>=0; i--){
+        for (int i=n-2; i>0; i--){
             dp[i] = sums[i] - maxi;
             maxi = max(maxi, dp[i]);
         }
 
-        return *max_element(dp.begin()+1, dp.end());
+        return maxi;
     }
 };
