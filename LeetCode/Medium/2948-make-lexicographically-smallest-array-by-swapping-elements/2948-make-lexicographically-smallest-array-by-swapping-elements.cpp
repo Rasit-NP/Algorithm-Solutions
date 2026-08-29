e270ed5888ed6871f26084492a29cb5cb41dc567
RASIT <53307918+Rasit-NP@users.noreply.github.com>
2026-08-30 03:55:15 +0900

    [Medium] Title: 2948-make-lexicographically-smallest-array-by-swapping-elements, Time: 106 ms (96.34%), Memory: 141.5 MB (86.48%) - LeetHub

---
+++ LeetCode/Medium/2948-make-lexicographically-smallest-array-by-swapping-elements/2948-make-lexicographically-smallest-array-by-swapping-elements.cpp
@@ -0,0 +1,50 @@
+# include <vector>
+# include <algorithm>
+using namespace std;
+typedef pair<int, int> pii;
+
+class Solution {
+private:
+    vector<int> res;
+
+    void adapt(vector<int>& position, vector<int>& value){
+        sort(position.begin(), position.end());
+        sort(value.begin(), value.end());
+
+        for (int i=0; i<position.size(); i++){
+            res[position[i]] = value[i];
+        }
+    }
+public:
+    vector<int> lexicographicallySmallestArray(vector<int>& nums, int limit) {
+        int n = nums.size();
+        res.assign(n, 0);
+        vector<pii> sortedNums;
+        for (int i=0; i<n; i++){
+            int num = nums[i];
+            sortedNums.emplace_back(num, i);
+        }
+
+        sort(sortedNums.begin(), sortedNums.end());
+
+        vector<vector<int>> positions, values;
+
+        vector<int> position, value;
+
+        for (int i=0; i<n; i++){
+            int num = sortedNums[i].first, idx = sortedNums[i].second;
+            if (position.size() && num - value.back() > limit) {
+                adapt(position, value);
+                position.clear();
+                value.clear();
+            }
+            position.emplace_back(idx);
+            value.emplace_back(num);
+        }
+        if (position.size()){
+            adapt(position, value);
+        }
+
+        return res;
+    }
+};
